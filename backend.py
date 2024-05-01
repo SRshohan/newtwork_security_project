1cacf5a8823d2f05d4b45646e13e469cfe0f31a5
Natasha Piedrabuena <natashapiedrabuena@Natashas-MacBook-Pro.local>
2024-05-01 16:22:00 -0400

    Changes

--- backend.py
+++ backend.py
@@ -1,50 +1,66 @@
 import sqlite3
 import bcrypt
+import re
+#small dict -> to check if its frequent password -> 
 
-def main():
+def createDB():
     conn = sqlite3.connect("Verification.db")
     cur = conn.cursor()
-    #cur.execute("DROP TABLE IF EXISTS User_Info") only to clear database
     cur.execute('''CREATE TABLE IF NOT EXISTS User_Info(
-        Username TEXT NOT NULL,
+         Email TEXT NOT NULL,
         Password BLOB NOT NULL,
-        Email TEXT NOT NULL,
-        PRIMARY KEY (Username),
-        CONSTRAINT unique_val UNIQUE (Username, Email))''')  
-    while True:
-        Input = int(input('Enter 1 - 4 for different options: '))
-        if Input == 1:
-            createAccount(cur)
-            conn.commit()  
-        elif Input == 2:
-            checkAccount(cur)
-        elif Input == 3:
-            cur.execute('''SELECT * FROM User_Info''')
-            results = cur.fetchall()  # Fetches all the results # Check if results exist and print them
-            if results:
-                for row in results:
-                    print(row)
-            else:
-                print("No records found.")
-        elif Input == 4:
-            break
-    conn.close()  
+        PRIMARY KEY (Email),
+        CONSTRAINT unique_val UNIQUE (Email))''')  
+    
+def dropTables():
+    conn = sqlite3.connect("Verification.db")
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS User_Info")
 
-def createAccount(cur):
-    username_input = input('Username: ')
-    password_input = input('Password: ')
-    email_input = input('Email: ')
+def createAccount():
+    conn = sqlite3.connect("Verification.db")
+    cur = conn.cursor()
+    email_input = validEmail()
+    password_input = validPassword()
     bytes = password_input.encode('utf-8')
     salt = bcrypt.gensalt()
-    encoded_password = bcrypt.hashpw(bytes, salt)
-    print(encoded_password)
-    cur.execute('''INSERT INTO User_Info (Username, Password, Email) VALUES (?, ?, ?)''',
-                (username_input, encoded_password, email_input))
+    encoded_password = bcrypt.hashpw(bytes, salt) #add the thing from sql lab
+    cur.execute('''INSERT INTO User_Info (Email, Password) VALUES (?, ?)''',
+                (email_input, encoded_password))
+    conn.commit() 
 
-def checkAccount(cur):
-    username_input = input('Username: ')
+def validEmail():
+    while True:
+        email_input = input('Email: ')
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email_input):
+                print("Invalid email format")
+        else:
+            return email_input
+def validPassword():
+    common_PW = {
+                "Password123@",
+                "Password123",
+                "AdminPassword1234",
+                "LetMeInNow123"
+                }
+    while True:
+        password_input = input('Password: ')
+        if (len(password_input) < 8 or not re.search(r"[A-Z]", password_input) or not re.search(r"[a-z]", password_input) or not re.search(r"[0-9]", password_input) or not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password_input)):
+            print("Try again...")
+            continue
+        else:
+            if password_input.lower() in common_PW:
+                print("Password is common. Try again...")
+                continue
+            else:
+                return password_input
+
+def checkAccount():
+    conn = sqlite3.connect("Verification.db")
+    cur = conn.cursor()
+    email_input = input('Email: ')
     password_input = input('Password: ')
-    cur.execute('''SELECT Password FROM User_Info WHERE Username = ?''', (username_input,))
+    cur.execute('''SELECT Password FROM User_Info WHERE Email = ?''', (email_input,))
     stored_password = cur.fetchone()
     if stored_password:
         user_bytes = password_input.encode('utf-8')
@@ -53,6 +69,23 @@ def checkAccount(cur):
         else:
             print('Incorrect password.')
     else:
-        print('Username not found.')
+        print('Email not found.')
 
+def adminInfo():
+    conn = sqlite3.connect("Verification.db")
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM User_Info''')
+    results = cur.fetchall()
+    if results:
+        for row in results:
+            print(row)
+    else:
+        print("No records found.")
+
+def main(): 
+    dropTables()
+    createDB()
+    createAccount()
+    checkAccount()
+    adminInfo()
 main()
